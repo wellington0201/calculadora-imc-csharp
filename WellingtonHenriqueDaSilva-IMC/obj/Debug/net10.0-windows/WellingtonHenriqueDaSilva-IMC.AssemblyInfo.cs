64290298f6b5289d90d982af701b4c47bb685437
Wellington <w.henriiique@gmail.com>
2026-03-28 14:09:46 -0300

    Projeto inicial: Calculadora de IMC

--- WellingtonHenriqueDaSilva-IMC/obj/Debug/net10.0-windows/WellingtonHenriqueDaSilva-IMC.AssemblyInfo.cs
+++ WellingtonHenriqueDaSilva-IMC/obj/Debug/net10.0-windows/WellingtonHenriqueDaSilva-IMC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WellingtonHenriqueDaSilva-IMC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f09f1b41f07d3f22c54fba701713f0e5f2762a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("WellingtonHenriqueDaSilva-IMC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WellingtonHenriqueDaSilva-IMC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
